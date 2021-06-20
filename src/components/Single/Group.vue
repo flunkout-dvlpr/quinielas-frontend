6d907ed319dfe5b2150999b825fdef852b2e6f8c
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-20 18:35:35 -0500

    Create a group component using q-card to display an instance of a group data object, the view displays essential props associated to the group such as name, number of members, list of members etc.

--- src/components/Single/Group.vue
+++ src/components/Single/Group.vue
@@ -1,18 +1,52 @@
 <template>
-  <div class="row justify-center items-center content-center">
-    <div class="col-12">
-      <div class="text-center text-h2 text-weight-thin">Group Card</div>
-    </div>
-  </div>
+  <q-card
+    square
+    :class="idx === selectedGroup ? 'bg-brand-5 border-dark-green-box shadow-8' : 'bg-brand-5 border-dark-green-box'"
+  >
+    <q-card-section class="q-pa-none q-ma-none">
+      <div class="row justify-center items-center content-center">
+        <div class="col-12 q-px-sm q-pt-xs q-pb-none">
+          <div class="text-h6 text-grey-3">
+            {{ group.name }}
+          </div>
+        </div>
+        <div class="col-12 q-px-sm q-pt-none q-pb-none">
+          <div class="text-caption text-grey-3">
+            Number of Members: {{ group.numberOfMembers }}
+          </div>
+        </div>
+      </div>
+    </q-card-section>
+    <q-card-section class="q-pa-none q-ma-none">
+      <div class="row justify-center items-center content-center">
+        <div class="col-11 q-ma-sm q-pa-sm bg-brand-4">
+          <div class="row justify-center items-center content-center">
+            <div class="col-12 q-pa-none">
+              <div class="text-caption text-grey-3">Members:</div>
+            </div>
+            <div
+              class="col-12 q-px-xs q-mb-xs bg-brand-5"
+              v-for="(member, idx) in group.members"
+              :key="idx"
+            >
+              <div class="text-caption text-grey-3">
+                {{ member.alias }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </q-card-section>
+  </q-card>
 </template>
 
 <script>
 export default {
   name: 'Group',
-  data () {
-    return {
-      loading: false
-    }
+  props: {
+    idx: Number,
+    selectedGroup: Number,
+    group: Object
   }
 }
 </script>
