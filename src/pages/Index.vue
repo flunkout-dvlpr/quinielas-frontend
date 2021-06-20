37f12d82bea3a19bad586c6af7ddafed79d5afd2
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-19 21:34:42 -0500

    Replace placeholder divs with router-view's pointing to route comps

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,29 +1,29 @@
 <template>
   <q-page>
-    <div class="row justify-center items-start content-start q-my-md">
+    <div class="row justify-center items-start content-start q-mt-md q-mb-sm">
       <div class="col-md-6 col-xs-12">
         <div class="row justify-center full-width">
-          <div style="height: 175px" class="col-12 bg-purple">
-            <div class="q-pa-md text-center text-h2 text-black">User</div>
+          <div class="col-11">
+            <router-view name="User" />
           </div>
-          <div style="height: 175px" class="col-12 bg-orange">
-            <div class="q-pa-md text-center text-h2 text-black">Groups</div>
+          <div style="height: 175px" class="col-11">
+            <router-view name="Groups" />
           </div>
         </div>
       </div>
       <div class="col-md-6 col-xs-12">
         <div class="row justify-center full-width">
-          <div style="height: 350px" class="col-12 bg-yellow">
-            <div class="q-pa-md text-center text-h2 text-black">Fixtures</div>
-            <div class="q-pa-md text-center text-h6 text-black">F1, ... FN, ... F10</div>
+          <div style="height: 350px" class="border-box col-11">
+            <router-view name="Fixtures" />
           </div>
         </div>
       </div>
-      <div class="col-md-12 col-xs-12">
+    </div>
+    <div class="row justify-start items-start content-start q-mt-none q-mb-sm">
+      <div class="col-md-6 col-xs-12">
         <div class="row justify-center full-width">
-          <div style="height: 350px" class="col-12 bg-green">
-            <div class="q-pa-md text-center text-h2 text-black">Matches</div>
-            <div class="q-pa-md text-center text-h6 text-black">M1-FN, ... MN-FN, ... MN-FN</div>
+          <div style="height: 350px" class="border-box col-11">
+            <router-view name="Matches" />
           </div>
         </div>
       </div>
@@ -36,3 +36,9 @@ export default {
   name: 'PageIndex'
 }
 </script>
+
+<style type="text/css">
+.border-box {
+  border-style: solid
+}
+</style>
