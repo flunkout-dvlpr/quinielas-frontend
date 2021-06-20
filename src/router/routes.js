7973e1d1453586bdff9039a7e8861cb5e2805722
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-19 21:34:03 -0500

    Modify Index route to use components referenced by reouter-view

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,7 +4,22 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') }
+      {
+        path: '',
+        component: () => import('pages/Index.vue'),
+        children: [
+          {
+            path: ':section?/',
+            name: 'Home',
+            components: {
+              User: () => import('components/Single/User.vue'),
+              Groups: () => import('components/Multiple/Groups.vue'),
+              Fixtures: () => import('components/Multiple/Fixtures.vue'),
+              Matches: () => import('components/Multiple/Matches.vue')
+            }
+          }
+        ]
+      }
     ]
   },
 
