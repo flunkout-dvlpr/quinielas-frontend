3c0b944bd22143979a2a7b71bebaa2e0e146bda5
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-19 21:36:12 -0500

    Components used to display a single instance of a data object

---
+++ src/components/Single/Fixture.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row justify-center items-center content-center">
+    <div class="col-12">
+      <div class="text-center text-h2 text-weight-thin">Fixture Card</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Fixture',
+  data () {
+    return {
+      loading: false
+    }
+  }
+}
+</script>
---
+++ src/components/Single/User.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="row justify-center items-center content-center brand-light-blue-gradient shadow-20">
+    <div class="col-12 full-width">
+      <q-item class="fit">
+        <q-item-section avatar>
+          <q-avatar :size="$q.platform.is.mobile ? '50px' : '75px'" color="brand-5" icon="person"/>
+        </q-item-section>
+        <q-item-section>
+          <q-item-label class="text-grey-3 text-h4">{{ user.alias }}</q-item-label>
+          <q-item-label  class="text-grey-3 text-h8" lines="1">{{ user.firstName }} {{ user.lastName }}</q-item-label>
+        </q-item-section>
+        <q-item-section side center>
+          <q-item-label class="text-grey-3 text-weight-regular text-h6">Win Ratio: {{ winRatio | winRatioFormat }}</q-item-label>
+          <q-item-label class="text-grey-3 text-weight-regular text-h6">Rank: {{ user.rank }}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  data () {
+    return {
+      user: {
+        alias: 'j.gent',
+        firstName: 'John',
+        lastName: 'Gentile',
+        lCount: 235,
+        wCount: 129,
+        rank: 1392
+      }
+    }
+  },
+  filters: {
+    winRatioFormat: function (winRatio) {
+      return `${Math.round(winRatio * 100)}%`
+    }
+  },
+  computed: {
+    winRatio () {
+      return this.user.wCount / (this.user.lCount + this.user.wCount)
+    }
+  }
+}
+</script>
