f561e217c57d3566d5b6523783312c3cd4a47b71
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-21 23:52:47 -0500

    Create AddGroup dialog to create and add groups to user, for the moment only the name is defined not the mebers or adding members

---
+++ src/components/Dialog/AddGroup.vue
@@ -0,0 +1,127 @@
+<template>
+  <q-dialog ref="dialog">
+  <q-card
+    style="width: 25rem;"
+    class="shadow-20 bg-brand-6"
+  >
+    <q-card-section class="q-pa-xs q-mb-md brand-green-gradient">
+      <div class="row justify-center items-center content-center">
+        <div class="col-md-12 col-xs-7 q-pa-sm">
+          <div class="text-center text-grey-3 text-h4 text-weight-regular">Add Group</div>
+        </div>
+      </div>
+    </q-card-section>
+    <!-- Group -->
+    <q-form @submit="onSubmit">
+    <q-card-section class="q-pa-sm">
+      <div class="row justify-center items-center content-center">
+        <div class="col-12 q-px-sm q-pt-none q-pb-xs">
+          <div class="">
+            <q-input
+              dark
+              square
+              outlined
+              color="brand-1"
+              v-model="group.name"
+              label="Group Name"
+            />
+          </div>
+        </div>
+      </div>
+    </q-card-section>
+    <!-- Tournament -->
+<!--     <q-card-section class="q-pa-sm">
+      <div class="row justify-center items-center content-center">
+        <div class="col-12 q-px-sm q-pt-none q-pb-xs">
+          <div class="">
+            <q-select
+              dark
+              square
+              outlined
+              color="brand-1"
+              :options="competitions"
+              v-model="selectedCompetition"
+              label="Select Competition"
+              readonly
+            />
+          </div>
+        </div>
+      </div>
+    </q-card-section> -->
+    <!-- Fixture -->
+<!--     <q-card-section class="q-pa-sm">
+      <div class="row justify-center items-center content-center">
+        <div class="col-12 q-px-sm q-pt-none q-pb-xs">
+          <div class="">
+            <q-select
+              dark
+              square
+              outlined
+              color="brand-1"
+              :options="fixtures"
+              v-model="selectedFixture"
+              label="Select Fixture"
+            />
+          </div>
+        </div>
+      </div>
+    </q-card-section> -->
+    <!-- Create Group -->
+    <q-card-section class="q-pa-sm q-ma-none bg-brand-6">
+      <div class="row justify-center items-center content-center">
+        <div class="col-4 q-px-sm q-py-none">
+          <q-btn
+            dark
+            square
+            no-caps
+            :ripple="false"
+            class="fit"
+            text-color="grey-3"
+            color="brand-2"
+            label="Submit"
+            type="submit"
+          />
+        </div>
+      </div>
+    </q-card-section>
+    </q-form>
+  </q-card>
+  </q-dialog>
+</template>
+<script>
+import { mapGetters, mapActions } from 'vuex'
+export default {
+  name: 'AddGroup',
+  data () {
+    return {
+      group: {
+        id: null,
+        name: null,
+        numberOfMembers: 0,
+        members: []
+      }
+    }
+  },
+  computed: {
+    ...mapGetters('groups', ['groups'])
+  },
+  methods: {
+    ...mapActions('groups', ['updateGroups']),
+    show () {
+      this.$refs.dialog.show()
+    },
+    hide () {
+      this.$refs.dialog.hide()
+    },
+    onCancel () {
+      this.hide()
+    },
+    onSubmit () {
+      const ids = this.groups.map(group => group.id).sort()
+      this.group.id = ids[ids.length - 1] + 1
+      this.updateGroups(this.group)
+      this.hide()
+    }
+  }
+}
+</script>
