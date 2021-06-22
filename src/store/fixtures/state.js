401874d63b842ec68063d17b9d830ec69a826209
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-21 19:00:46 -0500

    Replace tournament with competition in object keys

--- src/store/fixtures/state.js
+++ src/store/fixtures/state.js
@@ -1,20 +1,20 @@
 export default function () {
   return [
     {
-      id: 0,
+      id: 1,
       groupId: 1,
       name: 'Matchday 1',
-      tournamentId: null,
-      tournamentName: 'UEFA EURO 2020',
+      competitionId: null,
+      competitionName: 'UEFA EURO 2020',
       startDate: '06/11/21',
       endDate: '06/15/21'
     },
     {
-      id: 1,
+      id: 2,
       groupId: 3,
       name: 'Matchday 2',
-      tournamentId: null,
-      tournamentName: 'UEFA EURO 2020',
+      competitionId: null,
+      competitionName: 'UEFA EURO 2020',
       startDate: '06/16/21',
       endDate: '06/19/21'
     }
