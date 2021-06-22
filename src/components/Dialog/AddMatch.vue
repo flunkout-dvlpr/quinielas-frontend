5de2bd8cee1a09bf83c92ab1d71aaa35fcdcbf39
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-21 23:46:16 -0500

    Refactor dialog to use JSON API data to display the available matches based on the current selected group and fixture, add toggle buttons to define the prediction for the match W/L/D, pre-set the group/fixture selected

--- src/components/Dialog/AddMatch.vue
+++ src/components/Dialog/AddMatch.vue
@@ -12,6 +12,7 @@
       </div>
     </q-card-section>
     <!-- Fixture -->
+    <q-form @submit="onSubmit">
     <q-card-section class="q-pa-sm">
       <div class="row justify-center items-center content-center">
         <div class="col-12 q-px-sm q-pt-none q-pb-xs">
@@ -20,6 +21,7 @@
               dark
               square
               outlined
+              readonly
               color="brand-1"
               :options="fixtures"
               v-model="selectedFixture"
@@ -47,21 +49,49 @@
         </div>
       </div>
     </q-card-section>
-    <!-- Prediction -->
+    <!-- Team -->
     <q-card-section class="q-pa-sm q-ma-none bg-brand-6">
       <div class="row justify-center items-center content-center">
-        <div class="col-12 q-px-sm q-pb-xs text-weight-regular text-grey-3 text-h*">Prediction</div>
+        <!-- {{selectedMatch.match.awayTeam.name || ''}}  -->
+        <div class="col-12 q-px-sm q-pb-xs text-weight-regular text-grey-3 text-h*">{{ awayTeam }} Prediction</div>
         <div class="col-12 q-px-sm q-py-none">
           <q-btn-toggle
             spread
             no-caps
-            v-model="prediction"
-            color="brand-3"
+            emit-value
+            v-model="awayTeamPrediction"
+            @input="setAwayTeamPrediction"
             text-color="grey-3"
-            toggle-color="brand-1"
+            color="brand-6"
+            :toggle-color="awayTeamPrediction === 'W' ? 'green' : awayTeamPrediction === 'L' ? 'red' : 'orange'"
             :options="[
-              {label: 'Win', value: 'true', icon: 'done'},
-              {label: 'Lose', value: 'false', icon: 'close'}
+              {label: 'Win', value: 'W', icon: 'done'},
+              {label: 'Draw', value: 'D', icon: 'block'},
+              {label: 'Lose', value: 'L', icon: 'close'}
+            ]"
+          />
+        </div>
+      </div>
+    </q-card-section>
+    <!-- Prediction -->
+    <q-card-section class="q-pa-sm q-ma-none bg-brand-6">
+      <div class="row justify-center items-center content-center">
+        <!-- {{selectedMatch.match.homeTeam.name || ''}} -->
+        <div class="col-12 q-px-sm q-pb-xs text-weight-regular text-grey-3 text-h*">{{ homeTeam }} Prediction</div>
+        <div class="col-12 q-px-sm q-py-none">
+          <q-btn-toggle
+            spread
+            no-caps
+            emit-value
+            v-model="homeTeamPrediction"
+            @input="setHomeTeamPrediction"
+            text-color="grey-3"
+            color="brand-6"
+            :toggle-color="homeTeamPrediction === 'W' ? 'green' : homeTeamPrediction === 'L' ? 'red' : 'orange'"
+            :options="[
+             {label: 'Win', value: 'W', icon: 'done'},
+             {label: 'Draw', value: 'D', icon: 'block'},
+             {label: 'Lose', value: 'L', icon: 'close'}
             ]"
           />
         </div>
@@ -80,32 +110,18 @@
             text-color="grey-3"
             color="brand-2"
             label="Submit"
+            type="submit"
           />
         </div>
       </div>
     </q-card-section>
-
-    <!-- Date & Time -->
-<!--     <q-card-section class="q-pa-none q-ma-none">
-      <div class="row justify-center items-center content-center bg-brand-3">
-        <div class="col-6 column justify-end items-start q-px-sm">
-          <div class="text-h8 text-grey-3 text-center">
-            Date: {{ match.date }}
-          </div>
-        </div>
-        <div class="col-6 column justify-end items-end q-px-sm">
-          <div class="text-h8 text-grey-3 text-center">
-            @ {{ match.time }}
-          </div>
-        </div>
-      </div>
-    </q-card-section> -->
-
+    </q-form>
   </q-card>
   </q-dialog>
 </template>
 <script>
 import { date } from 'quasar'
+import { mapActions } from 'vuex'
 import euro from 'src/assets/euro.json'
 export default {
   name: 'AddMatch',
@@ -113,7 +129,8 @@ export default {
     return {
       selectedFixture: null,
       selectedMatch: null,
-      prediction: null,
+      awayTeamPrediction: 'W',
+      homeTeamPrediction: 'L',
       test: euro,
       fixtureOptions: [
         { value: 'm1', label: 'Matchday 1' },
@@ -143,7 +160,24 @@ export default {
       ]
     }
   },
+  props: {
+    fixtureId: Number
+  },
   computed: {
+    awayTeam () {
+      if (this.selectedMatch) {
+        return this.selectedMatch.match.awayTeam.name
+      } else {
+        return 'Away Team'
+      }
+    },
+    homeTeam () {
+      if (this.selectedMatch) {
+        return this.selectedMatch.match.homeTeam.name
+      } else {
+        return 'Home Team'
+      }
+    },
     fixtures () {
       const options = []
       this.test.matches.forEach(match => {
@@ -183,6 +217,31 @@ export default {
     }
   },
   methods: {
+    ...mapActions('matches', ['updateMatches']),
+    setHomeTeamPrediction (prediction) {
+      if (prediction === 'W') {
+        this.homeTeamPrediction = 'W'
+        this.awayTeamPrediction = 'L'
+      } else if (prediction === 'L') {
+        this.homeTeamPrediction = 'L'
+        this.awayTeamPrediction = 'W'
+      } else if (prediction === 'D') {
+        this.homeTeamPrediction = 'D'
+        this.awayTeamPrediction = 'D'
+      }
+    },
+    setAwayTeamPrediction (prediction) {
+      if (prediction === 'W') {
+        this.awayTeamPrediction = 'W'
+        this.homeTeamPrediction = 'L'
+      } else if (prediction === 'L') {
+        this.awayTeamPrediction = 'L'
+        this.homeTeamPrediction = 'W'
+      } else if (prediction === 'D') {
+        this.awayTeamPrediction = 'D'
+        this.homeTeamPrediction = 'D'
+      }
+    },
     show () {
       this.$refs.dialog.show()
     },
@@ -193,8 +252,35 @@ export default {
       this.hide()
     },
     onSubmit () {
-      // this.loading = true
+      const match = {
+        id: this.selectedMatch.match.id,
+        fixtureId: this.selectedFixture.value,
+        groupId: this.$route.params.groupId,
+        time: date.formatDate(date.extractDate(this.selectedMatch.match.utcDate, 'YYYY-MM-DDTHH:mm'), 'h:mma'),
+        date: date.formatDate(date.extractDate(this.selectedMatch.match.utcDate, 'YYYY-MM-DDTHH:mm'), 'MM/DD/YYYY'),
+        teamA: {
+          name: this.selectedMatch.match.awayTeam.id ? this.selectedMatch.match.awayTeam.name : 'TBA',
+          id: this.selectedMatch.match.awayTeam.id,
+          link: 'https://flagcdn.com/80x60/dk.png'
+        },
+        teamB: {
+          name: this.selectedMatch.match.homeTeam.id ? this.selectedMatch.match.homeTeam.name : 'TBA',
+          id: this.selectedMatch.match.homeTeam.id,
+          link: 'https://flagcdn.com/80x60/be.png'
+        },
+        scoreA: 0,
+        scoreB: 0,
+        predictionA: this.awayTeamPrediction,
+        predictionB: this.homeTeamPrediction,
+        winner: null
+      }
+      console.log(match)
+      this.updateMatches(match)
+      this.hide()
     }
+  },
+  mounted () {
+    this.selectedFixture = this.fixtures.find(fixture => fixture.value === this.fixtureId)
   }
 }
 </script>
