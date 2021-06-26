3510b0a4aff6e1c30427eeb2dd249bef28fb69ba
flunkout-dvlpr <info@juliogonzalez.space>
2021-06-25 21:47:35 -0500

    Refactor UI, add computed props to dynamically read route.params, handle showAddFixture if no fixtureId in route

--- src/components/Multiple/Fixtures.vue
+++ src/components/Multiple/Fixtures.vue
@@ -1,28 +1,43 @@
 <template>
   <q-card
-    class="shadow-20"
+    class="shadow-20 bg-brand-4"
   >
     <q-card-section class="q-pa-none q-ma-none brand-green-gradient">
-      <div class="row justify-center items-center content-center">
-        <div class="col-md-9 col-xs-7 q-pa-sm">
-          <div class="text-grey-3 text-h4 text-weight-regular">Fixtures</div>
+      <div
+        class="row justify-start items-center content-center"
+        style="height: 60px;"
+      >
+        <div class="q-mx-sm">
+          <q-avatar square class="bg-brand-3" font-size="40px" icon="account_tree" text-color="grey-3"/>
         </div>
-        <div class="col-md-3 col-xs-5 q-pa-sm">
-          <q-btn
-            outline
-            no-caps
-            :ripple="false"
-            label="Add Fixture"
-            class="fit text-grey-3"
-            @click="showAddFixtrueDialog()"
-          />
+        <div class="q-mx-sm">
+          <div class="text-grey-3 text-h4 text-weight-regular">Fixtures</div>
         </div>
       </div>
     </q-card-section>
-    <q-card-section class="q-pa-none q-ma-none bg-brand-4">
-      <div class="row justify-center items-center content-center q-pa-md">
+    <q-card-section class="q-pa-none q-ma-none">
+      <div
+        class="row justify-center items-start content-start hide-scrollbar q-pa-sm"
+        style="height: 300px;overflow-y: scroll;"
+      >
         <div
-          class="col-md-12 col-xs-12 q-ma-sm"
+          class="col-md-12 col-xs-12 column justify-center q-mx-none q-my-sm"
+          style="height: 75px;"
+        >
+          <div class="col border-dark-green-box-dash">
+            <q-btn
+              flat
+              no-caps
+              class="fit bg-brand-5"
+              @click="showAddFixtrueDialog()"
+            >
+              <q-icon size=20px name="add" color="grey-4"/>
+              <div class="text-h6 text-center text-weight-regular text-grey-4">Fixture</div>
+            </q-btn>
+          </div>
+        </div>
+        <div
+          class="col-md-12 col-xs-12 q-mx-none q-my-sm"
           v-for="fixture in groupFixtures"
           :key="fixture.id"
           @click="loadFixture(fixture.id)"
@@ -49,38 +64,80 @@ export default {
   },
   computed: {
     ...mapGetters('fixtures', ['fixtures']),
-    groupFixtures () {
+    groupId () {
       if (this.$route.params && this.$route.params.groupId !== null) {
-        console.log('Route Group ID', this.$route.params.groupId)
-        return this.fixtures.filter(fixture => parseInt(fixture.groupId) === parseInt(this.$route.params.groupId))
+        console.log('Fixtures.vue - groupId(): groupId Found In Route', this.$route.params.groupId)
+        return parseInt(this.$route.params.groupId)
+      } else {
+        console.log('Fixtures.vue - groupId(): groupId Not Found In Route', this.$route.params.groupId)
+        return null
       }
-      console.log('Error')
-      return this.fixtures
+    },
+    fixtureId () {
+      if (this.$route.params && this.$route.params.fixtureId !== null && !isNaN(this.$route.params.fixtureId)) {
+        console.log('Fixtures.vue - fixtureId(): fixtureId Found In Route', this.$route.params.fixtureId)
+        return parseInt(this.$route.params.fixtureId)
+      } else {
+        console.log('Fixtures.vue - fixtureId(): fixtureId Not Found In Route', this.$route.params.fixtureId)
+        return null
+      }
+    },
+    groupFixtures () {
+      if (this.groupId !== null) {
+        console.log('Fixtures.vue - groupFixtures(): returning fixtures with groupId', this.groupId)
+        return this.fixtures.filter(fixture => parseInt(fixture.groupId) === parseInt(this.groupId))
+      }
+      console.log('Fixtures.vue - groupFixtures(): groupId is null, return empty []')
+      return []
     }
   },
   methods: {
     loadFixture (idx) {
       this.selectedFixture = idx
-      if (this.$route.params && this.$route.params.groupId !== null) {
-        if (idx !== this.$route.params.fixtureId) {
-          this.$router.push({ name: 'Home', params: { groupId: this.$route.params.groupId, fixtureId: idx } })
+      if (this.groupId !== null && idx !== null) {
+        if (idx !== this.fixtureId) {
+          console.log('lFixtures.vue - oadFixture(): Set fixtureId route param to idx', idx)
+          this.$router.push({ name: 'Home', params: { groupId: this.groupId, fixtureId: idx } })
         }
       }
     },
     showAddFixtrueDialog () {
-      this.$q.dialog({
-        component: AddFixture,
-        parent: this,
-        groupId: this.$route.params.groupId
-      })
+      if (typeof this.groupId === 'number' && !isNaN(this.groupId)) {
+        this.$q.dialog({
+          component: AddFixture,
+          parent: this,
+          groupId: parseInt(this.groupId)
+        })
+      } else {
+        this.$q.notify({
+          color: 'orange-5',
+          position: 'top',
+          textColor: 'grey-9',
+          message: 'Please select a group first',
+          icon: 'group_add'
+        })
+      }
+    }
+  },
+  mounted () {
+    if (this.fixtureId !== null) {
+      console.log('Fixtures.vue - mounted(): fixtureId Found In fixtureId comp prop', this.fixtureId)
+      this.selectedFixture = this.fixtureId
+      this.loadFixture(this.fixtureId)
+    } else {
+      console.log('Fixtures.vue - mounted(): fixtureId Not Found In fixtureId comp prop', this.fixtureId)
+      this.selectedFixture = null
     }
   },
   updated () {
     this.$nextTick(function () {
-      // Select the first fixture if not fixture id is present in route params
-      console.log(this.$route.params.fixtureId)
-      if (this.groupFixtures.length && !this.$route.params.fixtureId) {
-        this.loadFixture(this.groupFixtures[0].id)
+      if (this.fixtureId !== null) {
+        console.log('Fixtures.vue - updated(): fixtureId Found In fixtureId comp prop', this.fixtureId)
+        this.selectedFixture = this.fixtureId
+        this.loadFixture(this.fixtureId)
+      } else {
+        console.log('Fixtures.vue - updated(): fixtureId Not Found In fixtureId comp prop', this.fixtureId)
+        this.selectedFixture = null
       }
     })
   }
