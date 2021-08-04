5f228e68d4b3b6354240892d1bfe5313b903c872
flunkout-dvlpr <info@juliogonzalez.space>
2021-08-03 23:58:31 -0500

    Add dispatch calls to load soccer/quinielas db data

--- src/store/member/actions.js
+++ src/store/member/actions.js
@@ -1,4 +1,17 @@
 
-export function loadMember ({ commit }) {
-  commit('setMember', user)
+export async function loadMember ({ commit, dispatch }, phone) {
+  return this._vm.$axios.get(`http://127.0.0.1:3000/member/get/by-phone/${phone}`).then((response) => {
+    if (response.data.type === 'success') {
+      const data = response.data.body
+      console.log(data)
+      commit('setMember', data.member)
+      commit('pools/setPools', data.pools, { root: true })
+      dispatch('fixtures/loadFixtures', null, { root: true })
+      dispatch('leagues/loadLeagues', null, { root: true })
+      dispatch('rounds/loadRounds', null, { root: true })
+      dispatch('poolRounds/loadPoolRounds', phone, { root: true })
+      dispatch('predictions/loadPredictions', phone, { root: true })
+      return data
+    }
+  })
 }
