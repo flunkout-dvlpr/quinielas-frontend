d96402945d933e482ba977faf985b805bb7db734
flunkout-dvlpr <info@juliogonzalez.space>
2021-08-04 15:58:06 -0500

    Conditionaly set axios base url, based on env prod or dev

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
 import axios from 'axios'
 
+if (process.env.PROD) {
+  axios.defaults.baseURL = 'https://59f5tguc6l.execute-api.us-east-2.amazonaws.com/Prod/'
+} else {
+  axios.defaults.baseURL = 'http://localhost:3000/'
+}
+
 Vue.prototype.$axios = axios
