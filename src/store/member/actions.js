06cbf3a8a23824bce17d76e7b564a3f10c43584c
flunkout-dvlpr <info@juliogonzalez.space>
2021-08-04 16:32:56 -0500

    Remove loading/setting pools from loadMember call and create/use loadPools action

--- src/store/member/actions.js
+++ src/store/member/actions.js
@@ -3,12 +3,14 @@ export async function loadMember ({ commit, dispatch }, phone) {
   return this._vm.$axios.get(`member/get/by-phone/${phone}`).then((response) => {
     if (response.data.type === 'success') {
       const data = response.data.body
-      console.log(data)
+      // Set SignedIn User
       commit('setMember', data.member)
-      commit('pools/setPools', data.pools, { root: true })
+      // Soccer Data
       dispatch('fixtures/loadFixtures', null, { root: true })
       dispatch('leagues/loadLeagues', null, { root: true })
       dispatch('rounds/loadRounds', null, { root: true })
+      // Quinielas Data
+      dispatch('pools/loadPools', phone, { root: true })
       dispatch('poolRounds/loadPoolRounds', phone, { root: true })
       dispatch('predictions/loadPredictions', phone, { root: true })
       return data
