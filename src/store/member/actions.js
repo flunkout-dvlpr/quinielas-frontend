e1dde903d985059ae53eecd3b582fbbd8ef30d5c
flunkout-dvlpr <info@juliogonzalez.space>
2021-08-04 18:19:37 -0500

    Add action to send otp to phone number

--- src/store/member/actions.js
+++ src/store/member/actions.js
@@ -1,4 +1,15 @@
 
+export async function signInOTP ({ commit, dispatch }, phone) {
+  // return { otp: 1234 }
+  return this._vm.$axios.post('authorization/sign-in/send-otp', { phone: phone }).then((response) => {
+    if (response.data.type === 'success') {
+      const data = response.data.body
+      console.log(data)
+      return data
+    }
+  })
+}
+
 export async function loadMember ({ commit, dispatch }, phone) {
   return this._vm.$axios.get(`member/get/by-phone/${phone}`).then((response) => {
     if (response.data.type === 'success') {
@@ -6,9 +17,9 @@ export async function loadMember ({ commit, dispatch }, phone) {
       // Set SignedIn User
       commit('setMember', data.member)
       // Soccer Data
-      dispatch('fixtures/loadFixtures', null, { root: true })
       dispatch('leagues/loadLeagues', null, { root: true })
       dispatch('rounds/loadRounds', null, { root: true })
+      dispatch('fixtures/loadFixtures', null, { root: true })
       // Quinielas Data
       dispatch('pools/loadPools', phone, { root: true })
       dispatch('poolRounds/loadPoolRounds', phone, { root: true })
